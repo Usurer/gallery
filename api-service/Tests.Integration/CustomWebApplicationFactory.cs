c26a7b1a5ebfcac1c104476444d6ead99b6441e7
Usurer <ivvlg@narod.ru>
2024-02-15 14:18:17 +0300

    simple test works

--- api-service/Tests.Integration/CustomWebApplicationFactory.cs
+++ api-service/Tests.Integration/CustomWebApplicationFactory.cs
@@ -1,17 +1,10 @@
-﻿using Core.Utils;
-using Database;
+﻿using Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests.Integration
 {
@@ -40,7 +33,7 @@ namespace Tests.Integration
                 // Create open SqliteConnection so EF won't automatically close it.
                 services.AddSingleton<DbConnection>(container =>
                 {
-                    var connection = new SqliteConnection("DataSource=:memory:");
+                    var connection = new SqliteConnection();
                     connection.Open();
 
                     return connection;
@@ -51,6 +44,8 @@ namespace Tests.Integration
                     var connection = container.GetRequiredService<DbConnection>();
                     options.UseSqlite(connection);
                 });
+
+                var sp = services.BuildServiceProvider();
             });
         }
     }
