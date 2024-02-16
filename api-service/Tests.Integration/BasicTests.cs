0b90eafd5b1730d2ef4d28b5e688ab7d653766d9
Usurer <ivvlg@narod.ru>
2024-02-16 15:17:55 +0300

    Sample tests with better data setup

--- api-service/Tests.Integration/BasicTests.cs
+++ api-service/Tests.Integration/BasicTests.cs
@@ -12,6 +12,45 @@ namespace Tests.Integration
         private readonly CustomWebApplicationFactory<Program> _factory;
         private readonly HttpClient client;
 
+        private IEnumerable<FileSystemItem> GenerateRootFolders(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return new FileSystemItem
+                {
+                    CreationDate = DateTimeUtils.ToUnixTimestamp(DateTime.Now),
+                    IsFolder = true,
+                    Name = $"Root Folder {i}",
+                    Path = $@"testpath:\subfolder\roots\folder_{i}",
+                };
+            }
+        }
+
+        private IEnumerable<FileSystemItem> GenerateChildren(long parentId, bool generateFolders, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var fileName = $"file_{i}.jpg";
+                var folderName = $"folder_{i}";
+                var name = generateFolders ? folderName : fileName;
+                var path = $@"testpath:\subfolder\roots\folder_{parentId}\{name}";
+
+                var item = new FileSystemItem
+                {
+                    CreationDate = DateTimeUtils.ToUnixTimestamp(DateTime.Now),
+                    IsFolder = generateFolders,
+                    Name = name,
+                    Path = path,
+                    Extension = generateFolders ? null : ".jpg",
+                    Height = generateFolders ? null : 100,
+                    Width = generateFolders ? null : 200,
+                    ParentId = parentId
+                };
+
+                yield return item;
+            }
+        }
+
         public BasicTests()
         {
             _factory = new CustomWebApplicationFactory<Program>();
@@ -22,23 +61,15 @@ namespace Tests.Integration
                 var scopedServices = scope.ServiceProvider;
                 var db = scopedServices.GetRequiredService<GalleryContext>();
 
-                db.FileSystemItems.Add(new FileSystemItem
-                {
-                    CreationDate = DateTimeUtils.ToUnixTimestamp(DateTime.Now),
-                    IsFolder = true,
-                    Name = "Test",
-                    Path = "Test path",
-                });
+                var roots = GenerateRootFolders(5).ToArray();
+
+                db.FileSystemItems.AddRange(roots);
                 db.SaveChanges();
 
-                db.FileSystemItems.Add(new FileSystemItem
+                foreach (var root in roots)
                 {
-                    CreationDate = DateTimeUtils.ToUnixTimestamp(DateTime.Now),
-                    IsFolder = true,
-                    Name = "Test 2",
-                    Path = "Test path 2",
-                    ParentId = 1
-                });
+                    db.FileSystemItems.AddRange(GenerateChildren(root.Id, false, 10));
+                }
                 db.SaveChanges();
             }
         }
@@ -56,11 +87,11 @@ namespace Tests.Integration
             var data = await response.Content.ReadFromJsonAsync<IEnumerable<FolderItemInfoModel>>();
 
             Assert.NotNull(data);
-            Assert.Equal(2, data.Count());
+            Assert.Equal(5 + 5 * 10, data.Count());
         }
 
         [Theory]
-        [InlineData("/folders/listItems/1")]
+        [InlineData("/folders/listItems/")]
         public async Task Folders_WhenListItems_ReturnsOk(string url)
         {
             // Arrange
@@ -72,7 +103,7 @@ namespace Tests.Integration
             var data = await response.Content.ReadFromJsonAsync<IEnumerable<FolderItemInfoModel>>();
 
             Assert.NotNull(data);
-            Assert.Single(data);
+            Assert.Equal(5, data.Count());
         }
     }
 }
