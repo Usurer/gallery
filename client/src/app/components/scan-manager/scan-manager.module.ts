29fb84afbdc37e4f798600851a2c13fe7e79a197
Usurer <ivvlg@narod.ru>
2024-02-20 15:13:06 +0300

    #16 Second sidenav for scans-manager

--- client/src/app/components/scan-manager/scan-manager.module.ts
+++ client/src/app/components/scan-manager/scan-manager.module.ts
@@ -6,6 +6,7 @@ import { AddScanComponent } from './add-scan/add-scan.component';
 
 @NgModule({
     declarations: [ScanManagerComponent, AddScanComponent],
+    exports: [ScanManagerComponent],
     imports: [CommonModule, HttpClientModule],
 })
 export class ScanManagerModule {}
