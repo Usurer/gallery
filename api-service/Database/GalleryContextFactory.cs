6cacd03739a1eab08fe11a71049ba5d98bc1eaf9
Usurer <ivvlg@narod.ru>
2024-03-02 19:48:44 +0300

    #23 Updates required for running migrations

---
+++ api-service/Database/GalleryContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Database;
+
+internal class GalleryContextFactory : IDesignTimeDbContextFactory<GalleryContext>
+{
+    public GalleryContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<GalleryContext>();
+        optionsBuilder.UseSqlite();
+
+        return new GalleryContext(optionsBuilder.Options);
+    }
+}
