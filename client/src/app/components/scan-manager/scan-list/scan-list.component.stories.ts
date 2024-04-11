be6f221511241550176591bf2bc4d237340637a3
Usurer <ivvlg@narod.ru>
2024-04-11 13:41:00 +0300

    #24: Story for ScanListComponent

--- client/src/app/components/scan-manager/scan-list/scan-list.component.stories.ts
+++ client/src/app/components/scan-manager/scan-list/scan-list.component.stories.ts
@@ -1,19 +1,45 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { ScanListComponent } from './scan-list.component';
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { FolderScan } from '../scan-manager.store';
+import { MatIconModule } from '@angular/material/icon';
+
+const mockScanned: FolderScan = {
+    id: 1,
+    path: 'Test 1',
+    isScanned: true,
+};
+
+const mockInvalid: FolderScan = {
+    id: 2,
+    path: 'Test 2',
+    isInvalid: true,
+};
+
+const mockNeither: FolderScan = {
+    id: 3,
+    path: 'Test 3',
+};
+
+const scansMocks = [mockScanned, mockInvalid, mockNeither];
 
 const meta: Meta<ScanListComponent> = {
     component: ScanListComponent,
     title: 'ScanListComponent',
+    decorators: [
+        moduleMetadata({
+            imports: [MatIconModule],
+        }),
+    ],
 };
 export default meta;
 type Story = StoryObj<ScanListComponent>;
 
 export const Primary: Story = {
     args: {
-        items: [],
+        items: scansMocks,
     },
 };
 
