bbe9e5b77fd02f886610670e1ebc30bca5473dbf
Usurer <ivvlg@narod.ru>
2024-06-21 13:14:22 +0300

    #23: Create initial DB migration

---
+++ api-service/Database/Migrations/20240621083418_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileSystemItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ParentId = table.Column<long>(type: "INTEGER", nullable: true),
+                    IsFolder = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CreationDate = table.Column<long>(type: "INTEGER", nullable: false),
+                    Extension = table.Column<string>(type: "TEXT", nullable: true),
+                    Width = table.Column<int>(type: "INTEGER", nullable: true),
+                    Height = table.Column<int>(type: "INTEGER", nullable: true),
+                    UpdatedAtDate = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileSystemItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScanTargets",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    IsScanned = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsInvalid = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScanTargets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileSystemItems");
+
+            migrationBuilder.DropTable(
+                name: "ScanTargets");
+        }
+    }
+}
